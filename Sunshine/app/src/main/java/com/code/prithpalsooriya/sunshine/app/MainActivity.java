962abfe1cd9f992acc1f69330e9de6a85d205ebd
Prithpal-Sooriya <prithpal.sooriya@gmail.com>
2016-08-28 23:59:03 +0100

    Completed first code for project sunshine
    Completed the first branch of project sunshine.

--- Sunshine/app/src/main/java/com/code/prithpalsooriya/sunshine/app/MainActivity.java
+++ Sunshine/app/src/main/java/com/code/prithpalsooriya/sunshine/app/MainActivity.java
@@ -1,14 +1,75 @@
 package com.code.prithpalsooriya.sunshine.app;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
+    //creation of activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //no saved instance state, so create the fragment
+        if (savedInstanceState == null) {
+            //create the fragment
+            getSupportFragmentManager().beginTransaction()
+                    //add the new fragment to the container (main layout)
+                    .add(R.id.container, new PlaceholderFragment())
+                    //commit to thee changes
+                    .commit();
+        }
     }
+
+    //creation of options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu, adds items to the actionbar if present
+        //get the XML for menu, and place inside the Menu menu
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //override of what happens when something in menu is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+         * Handle action bar item clicks here
+         * Action bar will handle clicks on Home/Up button so as long as
+         *      you specify parent activity in AndroidManifest.xml
+         */
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        //empty constructor
+        public PlaceholderFragment() {
+        }
+
+        //on create
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
 }
